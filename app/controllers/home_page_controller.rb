a2d57d62a5f4284674b566e745e737b251d33181
yvettetan <yvettetan97@gmail.com>
2021-09-14 01:53:04 +0800

    Fixes rubocop offenses

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,11 +2,8 @@ class HomePageController < ApplicationController
   skip_before_action :authenticate_user!, only: [:landing]
 
   def landing
-    if user_signed_in?
-      redirect_to home_page_index_path
-    end
+    redirect_to home_page_index_path if user_signed_in?
   end
 
-  def index
-  end
+  def index; end
 end
--- spec/system/create_user_spec.rb
+++ spec/system/create_user_spec.rb
@@ -1,47 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Account', type: :system, driver: :selenium_chrome, js: true do
-
   let(:fillout_form) do
     fill_in 'user_username', with: 'johndoe'
     fill_in 'user_email', with: 'johndoe@example.com'
     fill_in 'user_password', with: 'password'
     fill_in 'user_password_confirmation', with: 'password'
+    click_on 'Sign up'
   end
 
   describe 'Creating a valid account' do
-
     it 'visits sign up page' do
-        visit root_path
-        click_link 'Sign Up'
-        expect(page).to have_current_path(new_user_registration_path)
+      visit root_path
+      click_link 'Sign Up'
+      expect(page).to have_current_path(new_user_registration_path)
     end
 
     it 'fills out form with valid details' do
-        visit new_user_registration_path
-        fillout_form
-        click_on "Sign up"
-        expect(page).to have_content('Welcome! You have signed up successfully.')
-        expect(page).to have_current_path(home_page_index_path)
+      visit new_user_registration_path
+      fillout_form
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+
+    it 'redirects to home page' do
+      visit new_user_registration_path
+      fillout_form
+      expect(page).to have_current_path(home_page_index_path)
     end
   end
 
   describe 'Creating an invalid account' do
-
     it 'fills out form with registered username' do
-        create(:user)
-        visit new_user_registration_path
-        fillout_form
-        click_on "Sign up"
-        expect(page).to have_content('Username has already been taken')
+      create(:user)
+      visit new_user_registration_path
+      fillout_form
+      expect(page).to have_content('Username has already been taken')
     end
+
     it 'fills out form with registered email' do
-        create(:user)
-        visit new_user_registration_path
-        fillout_form
-        click_on "Sign up"
-        expect(page).to have_content('Email has already been taken')
+      create(:user)
+      visit new_user_registration_path
+      fillout_form
+      expect(page).to have_content('Email has already been taken')
     end
   end
-
 end
