cdb7ac0868f9b63d4e471651fda195e4c2a30c6b
Anna Gabriel <yssgabriel@gmail.com>
2021-09-14 20:42:37 +0800

    Add user room model #14

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_155524) do
+ActiveRecord::Schema.define(version: 2021_09_14_123443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2021_09_13_155524) do
     t.index ["preview_url"], name: "index_tracks_on_preview_url", unique: true
   end
 
+  create_table "user_rooms", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_user_rooms_on_room_id"
+    t.index ["user_id"], name: "index_user_rooms_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +66,6 @@ ActiveRecord::Schema.define(version: 2021_09_13_155524) do
 
   add_foreign_key "room_tracks", "rooms"
   add_foreign_key "room_tracks", "tracks"
+  add_foreign_key "user_rooms", "rooms"
+  add_foreign_key "user_rooms", "users"
 end
