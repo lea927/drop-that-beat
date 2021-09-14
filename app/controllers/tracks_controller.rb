6e127a2b6c804fec334a8112d379c2c9fc6a1c3e
lea927 <lxthym@icloud.com>
2021-09-14 23:32:40 +0800

    Connects to iTunes API

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,5 +4,12 @@ class TracksController < ApplicationController
   include HTTParty
 
   def index
+    get_preview_url
+  end
+
+  def get_preview_url
+    @term = 'levitating+dua+lipa'
+    request = HTTParty.get(ENV['BASE_URL']+'term='+@term+'&media=music&limit=5')
+    @request_hash = JSON.parse(request)
   end
 end
