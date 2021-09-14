4c75511f52c7faa6f8f492e8c3190667daf4f137
lea927 <lxthym@icloud.com>
2021-09-15 00:04:45 +0800

    Fixes merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root 'home_page#landing'
   get 'home_page/index'
-=======
   get 'tracks/index'
->>>>>>> preview
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
