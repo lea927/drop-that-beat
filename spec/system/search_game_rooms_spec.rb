6fd574ec79d5922eaeca2fd64923ddf9e53ed594
yvettetan <yvettetan97@gmail.com>
2021-09-17 21:19:26 +0800

    Adds test for invalid queries

--- spec/system/search_game_rooms_spec.rb
+++ spec/system/search_game_rooms_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'SearchGameRooms', type: :system do
   let(:user) { build(:user, points: 100) }
+  let(:track) { create(:track) }
   let(:room) { create(:room) }
 
   before do
@@ -10,6 +11,8 @@ RSpec.describe 'SearchGameRooms', type: :system do
     user.save!
     sign_in user
     room
+    track
+    room.tracks << track
     visit rooms_path
   end
 
@@ -20,6 +23,38 @@ RSpec.describe 'SearchGameRooms', type: :system do
 
     it 'shows search bar' do
       expect(page).to have_field('search')
+    end
+  end
+
+  context 'when user searches rooms with valid query' do
+    it 'shows rooms with associated query' do
+      fill_in 'search',	with: track.artist
+      click_on 'Search'
+      expect(page).to have_content(room.name)
+    end
+  end
+
+  context 'when user searches rooms with invalid query' do
+    it 'does not show rooms without associated query' do
+      fill_in 'search',	with: 'Bruno Mars'
+      click_on 'Search'
+      expect(page).not_to have_content(room.name)
+    end
+
+    it 'displays error message' do
+      expect(page).not_to have_content('Cannot find rooms with associated artist')
+    end
+  end
+
+  context 'when user searches with blank query' do
+    it 'shows all rooms' do
+      fill_in 'search',	with: ''
+      click_on 'Search'
+      expect(page).to have_content(room.name)
+    end
+
+    it 'displays error message' do
+      expect(page).not_to have_content('Please enter an artist to to search')
     end
   end
 end
