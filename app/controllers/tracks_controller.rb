dcbf45081c7a9bed144cc565afdac8fa642dc8d3
lea927 <lxthym@icloud.com>
2021-09-17 21:59:10 +0800

    Adds search path and template

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,6 +2,9 @@ class TracksController < ApplicationController
   include ApplicationHelper
 
   def index
+  end
+
+  def search_track
     search_track('new+rules+dua+lipa')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home_page#landing'
   get 'home_page/index'
   get 'tracks/index'
+  get '/search', to: 'tracks#search', as: :search
   devise_for :users
   resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
