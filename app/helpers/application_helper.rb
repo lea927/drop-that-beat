fe1f12c06f43832a183b8787e74338e8d99bcb50
lea927 <lxthym@icloud.com>
2021-09-17 21:48:59 +0800

    Fixes rubocop offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
   include HTTParty
 
   def search_track(term)
-    response = HTTParty.get(ENV['BASE_URL'] + "term=#{term}&media=music&limit=5")
-    @response_hash = JSON.parse(response)
+    HTTParty.get(ENV['BASE_URL'] + "term=#{term}&media=music&limit=5")
   end
 end
