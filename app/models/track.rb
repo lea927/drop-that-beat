cddec56a6a3c68f5d60176961f46a3a41fa998f1
yvettetan <yvettetan97@gmail.com>
2021-09-18 11:50:05 +0800

    Adds before save callback to downcase attributes

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,11 @@ class Track < ApplicationRecord
   validates :adam_id, :preview_url, uniqueness: true
   has_many :room_tracks, dependent: :destroy
   has_many :rooms, through: :room_tracks
+
+  before_save :set_downcase
+
+  def set_downcase
+    name.downcase!
+    artist.downcase!
+  end
 end
