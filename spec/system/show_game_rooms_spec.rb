f9396de22d89ebef6881dcb9462e941abc068d43
lea927 <lxthym@icloud.com>
2021-09-20 23:13:45 +0800

    Adds specs to validate path and content of show game room

---
+++ spec/system/show_game_rooms_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'ShowGameRooms', type: :system do
+  include_context 'when user is logged in'
+  let(:user) { build(:user) }
+  let(:room) { create(:room) }
+
+  before do
+    driven_by(:rack_test)
+    visit rooms_path
+  end
+
+  context 'when is user logged in and joins a room' do
+    it 'is on the correct path' do
+      expect(page).to have_current_path(new_user_registration_path)
+    end
+
+    it 'visits all rooms page' do
+      expect(page).to have_content 'All Music Rooms'
+    end
+  end
+end
