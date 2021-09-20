1fe8ec243c84275578d0db1f983329d61e4cd0d5
lea927 <lxthym@icloud.com>
2021-09-20 23:36:55 +0800

    Adds specsto search a room

--- spec/system/show_game_rooms_spec.rb
+++ spec/system/show_game_rooms_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'ShowGameRooms', type: :system do
   include_context 'when user is logged in'
   let(:user) { build(:user) }
-  let(:room) { create(:room) }
+  let(:track) { create(:track) }
+  let!(:room) { create(:room, tracks: [track]) }
 
   before do
     driven_by(:rack_test)
@@ -17,6 +18,12 @@ RSpec.describe 'ShowGameRooms', type: :system do
 
     it 'visits all rooms page' do
       expect(page).to have_content 'All Music Rooms'
+    end
+
+    it 'searches a room to join' do
+      fill_in 'search',	with: track.artist
+      click_on 'Search'
+      click_on room.name
     end
   end
 end
