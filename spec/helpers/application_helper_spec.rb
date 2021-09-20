98d7f7cd878d382d64215cee084bde695bc4ea96
Anna Gabriel <yssgabriel@gmail.com>
2021-09-20 16:49:28 +0800

    Specify method to build tracks from search results

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -22,4 +22,32 @@ RSpec.describe ApplicationHelper, :vcr, type: :helper do
       it { is_expected.to be_empty }
     end
   end
+
+  describe 'new_tracks', vcr: 'tracks/dua_lipa' do
+    subject(:new_tracks) { helper.new_track(tracks) }
+
+    let(:tracks) { helper.search_track(track) }
+
+    context 'with valid attributes' do
+      it 'build new track' do
+        expect(new_tracks.first).to be_instance_of(Track)
+      end
+
+      it 'maps all data' do
+        expect(new_tracks.count).to eq(tracks.count)
+      end
+    end
+
+    context 'with blank parameters' do
+      let(:track) { ' ' }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'with invalid tracks', vcr: 'invalid/tracks/unknown' do
+      let(:track) { 'asdfkjhasdflkjhas' }
+
+      it { is_expected.to be_nil }
+    end
+  end
 end
