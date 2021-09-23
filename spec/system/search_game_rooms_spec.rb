e0acaa14e26948c2d943cd1f8ca6222e1de68057
yvettetan <yvettetan97@gmail.com>
2021-09-23 14:37:39 +0800

    Adds new context to room model spec

--- spec/system/search_game_rooms_spec.rb
+++ spec/system/search_game_rooms_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe 'SearchGameRooms', type: :system do
   include_context 'when user is logged in'
   let(:track) { create(:track, artist: 'dua lipa') }
   let(:track2) { create(:track, artist: 'dua lipa', name: 'idgaf', adam_id: '1228739604', preview_url: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/dd/7c/f8/dd7cf8bf-61f2-45e3-9a52-63b7734d5ef1/mzaf_1015632960633637742.plus.aac.p.m4a') }
-  let!(:room) { create(:room, tracks: [track, track2]) }
+  let(:track3) { create(:track, artist: 'unordinary sunday, kino & yumin', name: 'UNORDINARY SUNDAY, Vol. 2 - Sunflower', adam_id: '1568415083', preview_url: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/67/4c/d7/674cd77b-8931-34a4-1148-7febd8ce042f/mzaf_13159739854727306776.plus.aac.p.m4a') }
+  let!(:room) { create(:room, tracks: [track, track2, track3]) }
 
   before do
     driven_by(:rack_test)
@@ -53,6 +54,14 @@ RSpec.describe 'SearchGameRooms', type: :system do
     it 'displays error message' do
       click_on 'Search'
       expect(page).to have_content('Please enter an artist to search')
+    end
+  end
+
+  context 'when user searches rooms with a featuring artist' do
+    it 'shows rooms with associated artist' do
+      fill_in 'search',	with: 'kino'
+      click_on 'Search'
+      expect(page).to have_content(/#{room.name}/i).once
     end
   end
 end
