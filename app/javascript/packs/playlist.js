8949280babd6f11cf1ff9c98ed358aea8fb3231e
Anna Gabriel <yssgabriel@gmail.com>
2021-09-24 22:40:24 +0800

    Add function to setup the tracks and create Audio object per track url

--- app/javascript/packs/playlist.js
+++ app/javascript/packs/playlist.js
@@ -1,10 +1,26 @@
 /** Class representing playlist */
 class Playlist {
-	/** Creates a new playlist */
+	/**
+	 * Creates a new playlist
+	 * @param {Array<string>} urlTracks Array of preview url
+	 */
 	constructor(urlTracks) {
 		this.trackNo = 0;
 		this.tracks = [];
 		this.currentTrack = '';
 		this.isPlaying = false;
+	}
+	/**
+	 * Setup playlist tracks
+	 * @param {Array<string>} urlTracks
+	 * @returns {Array.<Object>} Array of audio objects
+	 */
+	createTrack(urlTracks) {
+		urlTracks.forEach((urlTrack) => {
+			let track = new Audio(urlTrack);
+			track.addEventListener('ended', () => this.next());
+			this.tracks.push(track);
+		});
+		return this.tracks;
 	}
 }
