2648d3dcf2ebb742d2f40b80fdb328104cff647e
lea927 <lxthym@icloud.com>
2021-09-25 10:46:53 +0800

    Adds loader function on playlist JS and changes color of loader

--- app/javascript/packs/playlist.js
+++ app/javascript/packs/playlist.js
@@ -1,84 +1,105 @@
 /** Class representing playlist */
 class Playlist {
-	/**
-	 * Creates a new playlist
-	 * @param {Array<string>} urlTracks Array of preview url
-	 */
-	constructor(urlTracks) {
-		this.trackNo = 0;
-		this.tracks = [];
-		this.currentTrack = '';
-		this.isPlaying = false;
-		this.createTrack(urlTracks);
-	}
-	/**
-	 * Setup playlist tracks
-	 * @param {Array<string>} urlTracks
-	 * @returns {Array.<Object>} Array of audio objects
-	 */
-	createTrack(urlTracks) {
-		urlTracks.forEach((urlTrack) => {
-			let track = new Audio(urlTrack);
-			track.addEventListener('ended', () => this.next());
-			this.tracks.push(track);
-		});
-		return this.tracks;
-	}
-	/**
-	 * Play audio track. Track number is automatically determined
-	 * @returns {Playlist}
-	 */
-	play() {
-		this.tracks[this.trackNo].play();
-		this.currentTrack = this.tracks[this.trackNo];
-		this.isPlaying = true;
-		return this;
-	}
-	/**
-	 * Pause audio track.
-	 * @returns {Playlist}
-	 */
-	pause() {
-		this.tracks[this.trackNo].pause();
-		this.isPlaying = false;
-		return this;
-	}
-	/**
-	 * End audio track.
-	 * @returns {Playlist}
-	 */
-	end() {
-		this.tracks[this.trackNo].load();
-		this.currentTrack = '';
-		this.trackNo = 0;
-		this.isPlaying = false;
-		return this;
-	}
-	/**
-	 * Next Track
-	 * @returns {Playlist}
-	 */
-	next() {
-		this.tracks[this.trackNo].load();
-		this.nextTrackNo();
-		if (this.trackNo === 0) {
-			this.end();
-			return;
-		}
-		this.play();
-		return this;
-	}
-	/**
-	 * Sets the track number to the next track
-	 * @returns {Playlist}
-	 */
-	nextTrackNo() {
-		if (this.trackNo < this.tracks.length - 1) {
-			this.trackNo++;
-		} else {
-			this.trackNo = 0;
-		}
-		return this;
-	}
+  /**
+   * Creates a new playlist
+   * @param {Array<string>} urlTracks Array of preview url
+   */
+  constructor(urlTracks) {
+    this.trackNo = 0;
+    this.tracks = [];
+    this.currentTrack = '';
+    this.isPlaying = false;
+    this.createTrack(urlTracks);
+  }
+  /**
+   * Setup playlist tracks
+   * @param {Array<string>} urlTracks
+   * @returns {Array.<Object>} Array of audio objects
+   */
+  createTrack(urlTracks) {
+    urlTracks.forEach((urlTrack) => {
+      let track = new Audio(urlTrack);
+      track.addEventListener('ended', () => this.next());
+      track.addEventListener('play', () => this.loader());
+      this.tracks.push(track);
+    });
+    return this.tracks;
+  }
+  /**
+   * Play audio track. Track number is automatically determined
+   * @returns {Playlist}
+   */
+  play() {
+    this.tracks[this.trackNo].play();
+    this.currentTrack = this.tracks[this.trackNo];
+    this.isPlaying = true;
+    return this;
+  }
+  /**
+   * Pause audio track.
+   * @returns {Playlist}
+   */
+  pause() {
+    this.tracks[this.trackNo].pause();
+    this.isPlaying = false;
+    return this;
+  }
+  /**
+   * End audio track.
+   * @returns {Playlist}
+   */
+  end() {
+    this.tracks[this.trackNo].load();
+    this.currentTrack = '';
+    this.trackNo = 0;
+    this.isPlaying = false;
+    return this;
+  }
+  /**
+   * Next Track
+   * @returns {Playlist}
+   */
+  next() {
+    this.tracks[this.trackNo].load();
+    this.nextTrackNo();
+    if (this.trackNo === 0) {
+      this.end();
+      return;
+    }
+    this.play();
+    return this;
+  }
+  /**
+   * Sets the track number to the next track
+   * @returns {Playlist}
+   */
+  nextTrackNo() {
+    if (this.trackNo < this.tracks.length - 1) {
+      this.trackNo++;
+    } else {
+      this.trackNo = 0;
+    }
+    return this;
+  }
+
+  loader() {
+    let i = 0;
+    if (i == 0) {
+      i = 1;
+      var elem = document.getElementById('myBar');
+      var width = 1;
+      var id = setInterval(frame, 300);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          elem.style.width = width + '%';
+        }
+      }
+    }
+  }
 }
+
 export default Playlist;
