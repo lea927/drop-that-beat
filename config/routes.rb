840abc437af3a37bfff860ee57e04359deba5516
Anna Gabriel <yssgabriel@gmail.com>
2021-09-25 12:03:01 +0800

    Update the routes to include the endpoint to post the answer selected

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home', to: 'home_page#index'
   get 'tracks_json/:id', to: 'rooms#tracks_json', as: 'tracks_json'
   get '/search', to: 'tracks#search', as: :search
+  post '/rooms/:id', to: 'rooms#answer', as: 'answer_room'
   devise_for :users
   resources :rooms
   resources :tracks, except: %i[update]
