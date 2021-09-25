198551565535c2afdccf282c728146b0b0988d6e
yvettetan <yvettetan97@gmail.com>
2021-09-26 00:47:13 +0800

    Fixes tracks shuffle

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,8 +15,8 @@ class RoomsController < ApplicationController
 
   def show
     set_room_with_tracks
-    @tracks = @room.tracks if @room
-    @tracks_url = @tracks.map(&:preview_url).shuffle! if @tracks
+    @tracks = @room.tracks.to_a.shuffle! if @room
+    @tracks_url = @tracks.map(&:preview_url) if @tracks
   end
 
   def tracks_json
