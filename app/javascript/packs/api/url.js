eeb9677a02baf8bbe414045a065201eb6f6e1646
Anna Gabriel <yssgabriel@gmail.com>
2021-09-27 14:32:53 +0800

    Add tracks url path to fetch all tracks

--- app/javascript/packs/api/url.js
+++ app/javascript/packs/api/url.js
@@ -1,8 +1,11 @@
 /**
  * Class representing list of urls for API
  */
- class Url {
-  /** ROOMS URLS */
-  static get rooms() { return `/rooms.json` }
-  static room(id) { return `/rooms/${id}.json`}
- }
+class Url {
+	/** ROOMS URLS */
+	static get rooms() { return `/rooms.json` }
+	static room(id) { return `/rooms/${id}.json` }
+
+	/** TRACKS URLS */
+	static get tracks() { return `/tracks.json` }
+}
