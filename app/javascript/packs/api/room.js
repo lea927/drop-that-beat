ce80043ffd30b2ffc3b04376b2bece4718e0b534
Anna Gabriel <yssgabriel@gmail.com>
2021-09-27 14:59:15 +0800

    Add isInRoomPath function to check if the current path is the rooms_path

--- app/javascript/packs/api/room.js
+++ app/javascript/packs/api/room.js
@@ -4,4 +4,5 @@ import Url from './url';
 const roomsApi = {
   get rooms() { return get(Url.rooms) },
   room(id) { return get(Url.room(id)) },
+  isInRoomPath() { return window.location.pathname.includes('rooms') },
 };
