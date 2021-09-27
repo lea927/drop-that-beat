0d7b3ebb54f17d692cf13161ffe83075af6d4eb6
Anna Gabriel <yssgabriel@gmail.com>
2021-09-27 15:07:19 +0800

    Update room method to throw error if no room id is passed and the current room is not the rooms_path

--- app/javascript/packs/api/room.js
+++ app/javascript/packs/api/room.js
@@ -3,7 +3,10 @@ import Url from './url';
 
 const roomsApi = {
   get rooms() { return get(Url.rooms) },
-  room(id = roomsApi.setDefaultRoomId()) { return get(Url.room(id)) },
+  room(id = roomsApi.setDefaultRoomId()) {
+    if (!id) throw new Error('Missing room id argument'); // Not in rooms path and no id is indicated
+		return get(Url.room(id));
+	},
   isInRoomPath() { return window.location.pathname.includes('rooms') },
   getRoomIdFromPath() { return window.location.pathname.split('rooms').pop() },
 
