ba820d39282cfab15b751a57455128d01c18c608
Anna Gabriel <yssgabriel@gmail.com>
2021-09-28 18:47:13 +0800

    Add properties to track class

--- app/javascript/packs/model/track.js
+++ app/javascript/packs/model/track.js
@@ -1,2 +1,20 @@
 /** Class representing the tracks */
-class Track {}
+class Track {
+  /**
+   * Creates a new track
+   * @param {number} id 
+   * @param {string} name 
+   * @param {string} artist 
+   * @param {string} adam_id 
+   * @param {string} preview_url 
+   * @param {boolean} isCorrect 
+   */
+  constructor(id, name, artist, adam_id, preview_url, isCorrect ) {
+    this.id = id;
+    this.name = name;
+    this.artist = artist;
+    this.adam_id = adam_id;
+    this.preview_url = preview_url;
+    this.isCorrect = isCorrect;
+  }
+}
