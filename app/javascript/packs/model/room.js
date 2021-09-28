48857903e37ecd7168bf7aa2ac274ed5325a5c51
Anna Gabriel <yssgabriel@gmail.com>
2021-09-28 20:31:28 +0800

    Add name property to room class

--- app/javascript/packs/model/room.js
+++ app/javascript/packs/model/room.js
@@ -3,8 +3,10 @@ class Room {
 	/**
 	 * Creates a new room
 	 * @param {string} id - The room id
+	 * @param {string} name - The room name
 	 */
-	constructor(id) {
+	constructor(id, name) {
 		this.id = id;
+		this.name = name;
 	}
 }
