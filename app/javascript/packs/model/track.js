97c6561918fef9abf0a12ec61cbfb46e717986f4
Anna Gabriel <yssgabriel@gmail.com>
2021-09-28 18:54:53 +0800

    Add createCorrectTrack method to easily create correct track

--- app/javascript/packs/model/track.js
+++ app/javascript/packs/model/track.js
@@ -1,21 +1,29 @@
 /** Class representing the tracks */
 class Track {
-  /**
-   * Creates a new track
-   * @param {Object} track - the track object
-   * @param {number} track.id
-   * @param {string} track.name 
-   * @param {string} track.artist 
-   * @param {string} track.adam_id 
-   * @param {string} track.preview_url 
-   * @param {boolean} isCorrect
-   */
-   constructor({ id, name, artist, adam_id, preview_url }, isCorrect ) {
-    this.id = id;
-    this.name = name;
-    this.artist = artist;
-    this.adam_id = adam_id;
-    this.preview_url = preview_url;
-    this.isCorrect = isCorrect;
-  }
+	/**
+	 * Creates a new track
+	 * @param {Object} track - the track object
+	 * @param {number} track.id
+	 * @param {string} track.name
+	 * @param {string} track.artist
+	 * @param {string} track.adam_id
+	 * @param {string} track.preview_url
+	 * @param {boolean} isCorrect
+	 */
+	constructor({ id, name, artist, adam_id, preview_url }, isCorrect) {
+		this.id = id;
+		this.name = name;
+		this.artist = artist;
+		this.adam_id = adam_id;
+		this.preview_url = preview_url;
+		this.isCorrect = isCorrect;
+	}
+	/**
+	 * Creates a correct track by setting isCorrect property to true
+	 * @param {Object} track track object
+	 * @returns {Track}
+	 */
+	static createCorrectTrack(track) {
+		return new Track(track, (isCorrect = true));
+	}
 }
