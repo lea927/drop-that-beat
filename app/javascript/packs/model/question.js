c5fd488ebbf33da794fb49c48c1795d8e998137f
Anna Gabriel <yssgabriel@gmail.com>
2021-09-28 19:26:35 +0800

    Add track property to question class

--- app/javascript/packs/model/question.js
+++ app/javascript/packs/model/question.js
@@ -1,2 +1,10 @@
 /** Class representing the questions to be presented in the room */
-class Question {}
+class Question {
+  /**
+   * Creates a new question class
+   * @param {Object} track track details
+   */
+  constructor(track) {
+    this.track = track;
+  }
+}
