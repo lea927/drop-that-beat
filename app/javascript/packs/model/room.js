9246a8d4c92c867c613235adcfc0cddf551de4cb
Anna Gabriel <yssgabriel@gmail.com>
2021-09-28 20:32:08 +0800

    Add rounds property to room class

--- app/javascript/packs/model/room.js
+++ app/javascript/packs/model/room.js
@@ -4,9 +4,11 @@ class Room {
 	 * Creates a new room
 	 * @param {string} id - The room id
 	 * @param {string} name - The room name
+   * @param {integer} rounds - The number of rounds to be played
 	 */
-	constructor(id, name) {
+	constructor(id, name, rounds) {
 		this.id = id;
 		this.name = name;
+		this.rounds = rounds;
 	}
 }
