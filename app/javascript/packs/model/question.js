96c46b3789e5993263e0e1e2f0ddf4f51a0ddfa3
Anna Gabriel <yssgabriel@gmail.com>
2021-09-28 19:31:54 +0800

    Add getIncorrectTracks method to Question class
    This gets the incorrect tracks by removing the correct track from the list of tracks

--- app/javascript/packs/model/question.js
+++ app/javascript/packs/model/question.js
@@ -1,13 +1,24 @@
 /** Class representing the questions to be presented in the room */
 class Question {
-  /**
-   * Creates a new question class
-   * @param {Object} track track details(correct track)
-   * @param {Array<Object>} incorrectTracks array of incorrect tracks
-   */
-  constructor(track, incorrectTracks) {
-    this.track = track;
-    this.incorrectTracks = incorrectTracks;
-    this.noOfChoices = 3;
-  }
+	/**
+	 * Creates a new question class
+	 * @param {Object} track track details(correct track)
+	 * @param {Array<Object>} incorrectTracks array of incorrect tracks
+	 */
+	constructor(track, incorrectTracks) {
+		this.track = track;
+		this.incorrectTracks = incorrectTracks;
+		this.noOfChoices = 3;
+	}
+	/**
+	 * Get incorrect tracks
+	 * @param {number} indexOfCorrectTrack
+	 * @param {Array<Object>} tracks
+	 * @returns {Array<Object>} Object with track and incorrectTrack key
+	 */
+	static getIncorrectTracks(indexOfCorrectTrack, tracks) {
+		let incorrectTracks = [...tracks]; // Copy to prevent mutating array
+		incorrectTracks.splice(indexOfCorrectTrack, 1); // Remove correct track
+		return incorrectTracks;
+	}
 }
