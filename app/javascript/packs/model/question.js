fc666ef9bce8c6a658121c34afe5b3c44a11f70f
Anna Gabriel <yssgabriel@gmail.com>
2021-09-28 19:35:04 +0800

    Add create method to question class to easily create a question based on the returned track from the db

--- app/javascript/packs/model/question.js
+++ app/javascript/packs/model/question.js
@@ -10,6 +10,17 @@ class Question {
 		this.incorrectTracks = incorrectTracks;
 		this.noOfChoices = 3;
 	}
+  /**
+   * Create question class
+   * @param {Object} correctTrack 
+   * @param {number} indexOfCorrectTrack - the index of correct class is based on the list of tracks
+   * @param {Array<Object>} tracks - tracks in the room
+   * @returns {Question} question class
+   */
+  static create(correctTrack, indexOfCorrectTrack, tracks) {
+    let incorrectTracks = this.getIncorrectTracks(indexOfCorrectTrack, tracks);
+    return new Question(correctTrack, incorrectTracks);
+  }
 	/**
 	 * Get incorrect tracks
 	 * @param {number} indexOfCorrectTrack
