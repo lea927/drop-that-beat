a89c89f882f50f4f72d7f1b363bacc67f712f403
Anna Gabriel <yssgabriel@gmail.com>
2021-09-30 03:53:42 +0800

    Require game js to application js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require('../stylesheets/application.scss')
+require('./game')
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/scss/bootstrap'
 
