7bfe415627973df2e260a3a0cda039fa08b79622
Anna Gabriel <yssgabriel@gmail.com>
2021-09-30 03:54:34 +0800

    Format document using prettifier

--- app/javascript/packs/loader.js
+++ app/javascript/packs/loader.js
@@ -1,37 +1,35 @@
 /**
- * @fileoverview This contains the functions to create the progress bar/loader 
+ * @fileoverview This contains the functions to create the progress bar/loader
  * for the music app
  */
 
-// ==================================================
-//           FUNCTIONS
-// ==================================================
-
 export function hideLoader() {
-  $('#myBar').addClass('hide');
-  $('#myProgress').addClass('hide');
+	$('#myBar').addClass('hide');
+	$('#myProgress').addClass('hide');
 }
 
 export function showLoader() {
-  $('#myBar').removeClass('hide').addClass('show');
-  $('#myProgress').removeClass('hide').addClass('show');
+	$('#myBar').removeClass('hide').addClass('show');
+	$('#myProgress').removeClass('hide').addClass('show');
 }
 
 export function loader() {
-  let loader = document.getElementById('myBar');
-  let width = 1;
-  let id = setInterval(frame, 300);
-  function frame() {
-    if (width >= 100) { return clearInterval(id) };
-    width++;
-    loader.style.width = width + '%';
-  }
+	let loader = document.getElementById('myBar');
+	let width = 1;
+	let id = setInterval(frame, 300);
+	function frame() {
+		if (width >= 100) {
+			return clearInterval(id);
+		}
+		width++;
+		loader.style.width = width + '%';
+	}
 }
 
 export function checkPlaylistStatus(playlist) {
-  setInterval(function () {
-    if (playlist.isPlaying === false) {
-      hideLoader();
-    }
-  }, 1000);
+	setInterval(function () {
+		if (playlist.isPlaying === false) {
+			hideLoader();
+		}
+	}, 1000);
 }
