af7cc3459ad766a7c872213aff91d960771d7076
Anna Gabriel <yssgabriel@gmail.com>
2021-09-30 02:06:53 +0800

    Create game class and add all functions

--- app/javascript/packs/game.js
+++ app/javascript/packs/game.js
@@ -1,77 +1,79 @@
-function addStartBtnListener() {
-  const startBtn = document.querySelector("#startGameBtn");
-  startBtn?.addEventListener("click", startGame);
-}
-
-function startGame(e) {
-  e.preventDefault();
-  displayGame();
-  playlist.play();
-  startBtn.style.display = "none";
-}
-function shuffle(array) {
-  let currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
+/** Class representing the game */
+class Game {
+  addStartBtnListener() {
+    const startBtn = document.querySelector("#startGameBtn");
+    startBtn?.addEventListener("click", startGame);
   }
-  return array;
-}
-
-function displayGame() {
-  const question = document.querySelector("#question");
-  question.style.visibility = "visible";
-  question.innerText = `Guess the song:`;
-  getData();
-}
-
-async function getData() {
-  await fetch(`/tracks_json/${document.getElementById("room").dataset.url}`)
-    .then((res) => res.json())
-    .then((data) => {
-      buildChoices(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
-function buildChoices(data) {
-  let incorrectTracks = [];
-  let correctTrack;
-  data.forEach((track) => {
-    if (track.preview_url != playlist.currentTrack.src) {
-      incorrectTracks.push(track);
-    } else {
-      correctTrack = track;
+  startGame(e) {
+    e.preventDefault();
+    displayGame();
+    playlist.play();
+    startBtn.style.display = "none";
+  }
+  shuffle(array) {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
-  });
-  let choices = generateRandomChoices(incorrectTracks, correctTrack);
-  displayChoices(choices);
-  isEnded(data);
-}
+    return array;
+  }
 
-function generateRandomChoices(incorrectTracks, correctTrack) {
-  let wrongChoices = shuffle(incorrectTracks).slice(0, 2);
-  let choices = [...wrongChoices, correctTrack];
-  return shuffle(choices);
-}
+  displayGame() {
+    const question = document.querySelector("#question");
+    question.style.visibility = "visible";
+    question.innerText = `Guess the song:`;
+    getData();
+  }
 
-function displayChoices(choices) {
-  const choiceBtns = document.querySelectorAll(".choiceBtn");
-  choiceBtns.forEach((choiceBtn, i) => {
-    choiceBtn.textContent = `${choices[i].name} by ${choices[i].artist}`;
-    choiceBtn.style.display = "block";
-  });
-}
+  async getData() {
+    await fetch(`/tracks_json/${document.getElementById("room").dataset.url}`)
+      .then((res) => res.json())
+      .then((data) => {
+        buildChoices(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-function isEnded(data) {
-  playlist.tracks.forEach((track) => {
-    track.addEventListener("ended", () => buildChoices(data));
-  });
+  buildChoices(data) {
+    let incorrectTracks = [];
+    let correctTrack;
+    data.forEach((track) => {
+      if (track.preview_url != playlist.currentTrack.src) {
+        incorrectTracks.push(track);
+      } else {
+        correctTrack = track;
+      }
+    });
+    let choices = generateRandomChoices(incorrectTracks, correctTrack);
+    displayChoices(choices);
+    isEnded(data);
+  }
+
+  generateRandomChoices(incorrectTracks, correctTrack) {
+    let wrongChoices = shuffle(incorrectTracks).slice(0, 2);
+    let choices = [...wrongChoices, correctTrack];
+    return shuffle(choices);
+  }
+
+  displayChoices(choices) {
+    const choiceBtns = document.querySelectorAll(".choiceBtn");
+    choiceBtns.forEach((choiceBtn, i) => {
+      choiceBtn.textContent = `${choices[i].name} by ${choices[i].artist}`;
+      choiceBtn.style.display = "block";
+    });
+  }
+
+  isEnded(data) {
+    playlist.tracks.forEach((track) => {
+      track.addEventListener("ended", () => buildChoices(data));
+    });
+  }
 }
