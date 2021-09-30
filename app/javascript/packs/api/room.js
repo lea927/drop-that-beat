d772744909d085c8cb51654d69be129aad5a9ae7
Anna Gabriel <yssgabriel@gmail.com>
2021-09-30 20:13:45 +0800

    Add postAnswer function to roomsApi module

--- app/javascript/packs/api/room.js
+++ app/javascript/packs/api/room.js
@@ -7,6 +7,16 @@ const roomsApi = {
     if (!id) throw new Error('Missing room id argument'); // Not in rooms path and no id is indicated
 		return get(Url.room(id));
 	},
+	/**
+	 * Post answers which adds points to the user
+	 * @param {number} roomId
+	 * @param {Object} data
+	 * @returns {Promise} result of the request ({boolean} true if the answer is correct )
+	 */
+   postAnswer(roomId, data) {
+		let path = Url.answer(roomId);
+		return post(path, { data: data });
+	},
   isInRoomPath() { return window.location.pathname.includes('rooms') },
   getRoomIdFromPath() { return window.location.pathname.split('rooms').pop() },
 
