cf0d1eb9857eb85c851efad41cecf3021f90a22b
Anna Gabriel <yssgabriel@gmail.com>
2021-10-01 15:17:16 +0800

    Add stubbed tracks to rooms data

--- test/factories/rooms.rb
+++ test/factories/rooms.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :room do
     name { 'animal' }
     tracks { build_list(:track, 5) }
+    trait :stub_tracks do
+      tracks { build_stubbed_list(:track, 5) }
+    end
   end
 end
