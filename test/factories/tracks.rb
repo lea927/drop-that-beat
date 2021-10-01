79628a2c8153151fb683a93c9205ca4fcba2f52a
Anna Gabriel <yssgabriel@gmail.com>
2021-10-01 12:32:07 +0800

    Update track test to increment preview_url everytime it is created

--- test/factories/tracks.rb
+++ test/factories/tracks.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { 'Levitating' }
     artist { 'Dua Lipa' }
     sequence(:adam_id) { |n| "155117940#{n}" }
-    preview_url { 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/ab/a4/60/aba460c2-787f-7c31-ecbe-b2f50b4e4725/mzaf_12011994877481871015.plus.aac.p.m4a' }
+    sequence(:preview_url) { |n| "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/ab/a4/60/aba460c2-787f-7c31-ecbe-b2f50b4e4725/mzaf_12011994877481871015#{n}.plus.aac.p.m4a" }
     trait :rooms do
       tracks { [association(:room)] }
     end
