10722b1dba935829cb21b29acae37534ca1e8ba7
lea927 <lxthym@icloud.com>
2021-10-05 20:34:55 +0800

    Corrects devise rb file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -27,7 +27,9 @@ Devise.setup do |config|
   config.mailer_sender = ENV['MAILER_EMAIL']
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'Devise::Mailer'
+  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'DeviseCustomMailer'
+
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
