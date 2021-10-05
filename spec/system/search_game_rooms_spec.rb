d6b3a3c3d09a0983492332d18b69aac54afbfbce
Anna Gabriel <yssgabriel@gmail.com>
2021-10-05 22:14:31 +0800

    Update search game room spec to create valid and invalid rooms Valid rooms are rooms not created by user and with tracks Invalid rooms are rooms created by user or rooms without tracks

--- spec/system/search_game_rooms_spec.rb
+++ spec/system/search_game_rooms_spec.rb
@@ -2,18 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'SearchGameRooms', type: :system do
   include_context 'when user is logged in'
+  let(:user2) { create(:user, username: 'janedoe', email: 'janedoe@example.com') }
   let(:track) { create(:track, artist: 'unordinary sunday, kino & yumin', name: 'UNORDINARY SUNDAY, Vol. 2 - Sunflower', adam_id: '1568415083', preview_url: 'https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview115/v4/67/4c/d7/674cd77b-8931-34a4-1148-7febd8ce042f/mzaf_13159739854727306776.plus.aac.p.m4a') }
-  let!(:room) { create(:room) }
+  let!(:invalid_room) { create(:room, name: 'BTS', users: [user]) }
+  let!(:valid_room) do
+    user2.confirm
+    create(:room, users: [user2])
+  end
 
   before do
     driven_by(:rack_test)
-    room.tracks.push(track)
+    valid_room.tracks.push(track)
     visit rooms_path
   end
 
   context 'when user is logged in and views all rooms' do
-    it 'shows list of rooms created' do
-      expect(page).to have_content(/#{room.name}/i).once
+    it 'shows list of playable rooms' do
+      expect(page).to have_content(/#{valid_room.name}/i).once
+    end
+
+    it 'does not display rooms created' do
+      expect(page).not_to have_content(invalid_room.name)
     end
 
     it 'shows search bar' do
@@ -25,7 +34,7 @@ RSpec.describe 'SearchGameRooms', type: :system do
     it 'shows rooms with associated query' do
       fill_in 'search',	with: track.artist
       click_on 'Search'
-      expect(page).to have_content(/#{room.name}/i).once
+      expect(page).to have_content(/#{valid_room.name}/i).once
     end
   end
 
@@ -33,7 +42,7 @@ RSpec.describe 'SearchGameRooms', type: :system do
     it 'does not show rooms without associated query' do
       fill_in 'search',	with: 'Bruno Mars'
       click_on 'Search'
-      expect(page).not_to have_content(/#{room.name}/i)
+      expect(page).not_to have_content(/#{valid_room.name}/i)
     end
 
     it 'displays error message' do
@@ -47,7 +56,7 @@ RSpec.describe 'SearchGameRooms', type: :system do
     it 'shows all rooms' do
       fill_in 'search',	with: ''
       click_on 'Search'
-      expect(page).to have_content(/#{room.name}/i)
+      expect(page).to have_content(/#{valid_room.name}/i)
     end
 
     it 'displays error message' do
@@ -60,7 +69,7 @@ RSpec.describe 'SearchGameRooms', type: :system do
     it 'shows rooms with associated artist' do
       fill_in 'search',	with: 'kino'
       click_on 'Search'
-      expect(page).to have_content(/#{room.name}/i).once
+      expect(page).to have_content(/#{valid_room.name}/i).once
     end
   end
 end
